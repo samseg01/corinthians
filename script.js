be9ec4dce963c571ea5c28f012ef2570b37b93a3
Samuel Seguins <samuelseguins@gmail.com>
2025-01-03 20:45:04 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const hino = new Audio('hino.mp3');
+const efeito = new Audio('Corinthians vinheta da Radio! Corinthians.mp3')
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    hino.currentTime = 0;
+    hino.play();
+})
+
+document.getElementById('logo').addEventListener('click', ()=>{
+    efeito.currentTime = 3.66;
+    efeito.play();
+})
